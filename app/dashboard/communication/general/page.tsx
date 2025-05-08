34306fc17bc2fad614237aad7140b8ba60bb3c2c
adeleye remi-alarape <adeleye.remi-alarape@AI-4AI.org>
2025-05-08 03:19:58 +0100

    communication feature

---
+++ app/dashboard/communication/general/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { GeneraltList } from "@/components/admin/general"
+import { useAuth } from "@/contexts/auth-context"
+
+const CommunicationView = () => {
+  const { user } = useAuth()
+
+  if (!user || user.role !== "superadmin") {
+    return null // or show a fallback/403 component
+  }
+
+  return <GeneraltList />
+}
+
+export default CommunicationView
